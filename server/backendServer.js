28d058b612d013f6ccc89e4c3b3329ae76df19d8
ericgrosse <ericgrosse1@gmail.com>
2016-08-02 12:17:25 -0400

    Added a basic error handler to the backend

--- server/backendServer.js
+++ server/backendServer.js
@@ -16,6 +16,22 @@ app.use(logger('dev'));
 require('./mongoose')();
 app.use('/api', routes);
 
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// The error handler itself
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
 app.listen(port, function() {
   console.log('\nBackend server is running on port %s\n'.green, port);
 });
