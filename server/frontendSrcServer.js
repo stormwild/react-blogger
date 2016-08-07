ba0d5fd4ef2a3840605a54356bacad3954534ff4
ericgrosse <ericgrosse1@gmail.com>
2016-08-07 05:13:35 -0400

    Toggling hot reload off allows for full reloading (still in progress)

--- server/frontendSrcServer.js
+++ server/frontendSrcServer.js
@@ -8,33 +8,44 @@ var webpackConfig = require('../webpack.config.dev');
 var open = require('open');
 var proxy = require('http-proxy-middleware');
 var colors = require('colors');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var WebpackDevServer = require('webpack-dev-server');
+var compiler = webpack(webpackConfig);
 
 var frontendPort = 3000;
 var backendPort = 5000;
 var app = express();
-var compiler = webpack(webpackConfig);
-
-app.set('view engine', 'ejs');
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath
-}));
 
 if (config.hotReloadEnabled) {
+  app.set('view engine', 'ejs');
+
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+
   app.use(require('webpack-hot-middleware')(compiler));
+
+  // Forward all requests to a separate server. By doing this, the backend can use nodemon without rebuilding the webpack bundle on each server file change.
+  app.use('*', proxy({
+    target: 'http://localhost:' + backendPort
+  }));
+
+  app.listen(frontendPort, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('\nFrontend dev server is running on port %s\n'.green, frontendPort);
+      open('http://localhost:' + frontendPort);
+    }
+  });
 }
+else {
+  webpackConfig.entry.unshift('webpack-dev-server/client?http://localhost:8080');
 
-// Forward all requests to a separate server. By doing this, the backend can use nodemon without rebuilding the webpack bundle on each server file change.
-app.use('*', proxy({
-  target: 'http://localhost:' + backendPort
-}));
-
-app.listen(frontendPort, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('\nFrontend dev server is running on port %s\n'.green, frontendPort);
-    open('http://localhost:' + frontendPort);
-  }
-});
+  var server = new WebpackDevServer(compiler, {
+    contentBase: './src',
+    quiet: true
+  });
+  server.listen(8080);
+}
